b06c39c804e7db2fef17c831474657b7dfddf7b8
Moriya Dechtiar <mod821@g.harvard.edu>
2020-12-08 11:14:06 -0500

    completed function methods

--- auto_diff_pkg/ReverseAutoDiff.py
+++ auto_diff_pkg/ReverseAutoDiff.py
@@ -1,102 +1,207 @@
 import numpy as np
 
 class ReverseAutoDiff():
+    def __init__(self, variables, functions, input_values):
+        self.graph = []
+        self.variables = variables
+        self.functions = functions
+    
 
-    def __init__(self, value, variables = 1, position = 0):
-        self.val = value
+class ReverseADNode:
+    def __init__(self, value):
+        self.value = value
         self.children = []
         self.grad_value = None
         self.op = ''
 
-    def __neg__(self):
-        node = ReverseAutoDiff(-self.val)
-        node.children.append((-self.der, self))
-        node.op = 'neg'
-        return node
-
+    def grad(self):
+        if self.grad_value is None:
+            self.grad_value = sum(weight * var.grad() for weight, var in self.children)
+        return self.grad_value
+    
     def __add__(self, other):
-        try:
-            node = ReverseAutoDiff(self.val+other.val)
-            node.children.append((1.0, other))
-            node.children.append((1.0, self))
-        except AttributeError:
-            node = ReverseAutoDiff(self.val+other)
-            other_node = ReverseAutoDiff(other)
-            node.children.append((0.0, other_node))
-            node.children.append((1.0, self))
-        node.op = 'add'
+        node = ReverseADNode(self.value + other.value)
+        self.children.append((1.0, node))
+        other.children.append((1.0, node))
+        node.op = 'ADD'
         return node
-            
-        
-    def __radd__(self, other): 
-        return self.__add__(other)
-          
-    def __mul__(self, other):
-        try:
-            node = ReverseAutoDiff(self.val*other.val)
-            node.children.append((other.val, self))
-            node.children.append((self.val, other))
-        except AttributeError:
-            node = ReverseAutoDiff(self.val*other)
-            other_node = ReverseAutoDiff(other)
-            node.children.append((other_node.val, self))
-            node.children.append((0.0, other_node))
-        node.op = 'mul'
-        return node
-
-    def __rmul__(self, other):
-        return self.__mul__(other)
     
     def __sub__(self, other):
-        try:
-            node = ReverseAutoDiff(self.val-other.val)
-            node.children.append((1.0, self))
-            node.children.append((-1.0, other))
-        except AttributeError:
-            node = ReverseAutoDiff(self.val-other)
-            other_node = ReverseAutoDiff(other)
-            node.children.append((1.0, self))
-            node.children.append((0, other_node))
-        node.op = 'sub'
+        if not isinstance(other, ReverseADNode):
+            other = ReverseADNode(other)
+        node = ReverseADNode(self.value - other.value)
+        self.children.append((1.0, node))
+        other.children.append((-1.0, node))
+        node.op = 'SUB'
         return node
     
-    def __rsub__(self, other): 
-        node = ReverseAutoDiff(other - self.val)
-        other_node = ReverseAutoDiff(other)
-        node.children.append((-1.0, self))
-        node.children.append((0, other_node))
-        node.op = 'sub'
+    def __rsub__(self, other):
+        other = ReverseADNode(other)
+        node = ReverseADNode(other.value - self.value)
+        self.children.append((-1.0, node))
+        other.children.append((1.0, node))
+        node.op = 'SUB'
         return node
     
-    def reverse_mode(self):
-        # recurse only if the value is not yet cached
-        if self.grad_value is None:
-            self.grad_value = 0
-            # calculate derivative using chain rule
-            if len(self.children) == 0:
-                return 1
-            for der, node in self.children:
-                self.grad_value += (der * node.reverse_mode())
-        return self.grad_value
+    def __inv__(self):
+        node = ReverseADNode(1 / self.value)
+        self.children.append(-self.value ** -2, node)
+        node.op = 'INV'
+        return node
+                     
+    def __truediv__(self, other):
+        if not isinstance(other, ReverseADNode):
+            other = ReverseADNode(other)
+        return self.__mul__(other.__inv())
+    
+    def __rtruediv__(self, other):
+        other = ReverseADNode(other)
+        return other.__mul__(self.__inv())
+    
+    def __mul__(self, other):
+        node = ReverseADNode(self.value * other.value)
+        self.children.append((other.value, node))
+        other.children.append((self.value, node))
+        node.op = 'MUL/DIV'
+        return node
+    
+    def __pow__(self, other):
+        if not isinstance(other, ReverseADNode):
+            other = ReverseADNode(other)
+        node = ReverseADNode(self.value ** other.value)
+        self.children.append((other.value*self.value**(other.value-1), node))
+        other.children.append(((self.value**other.value)*np.log(np.abs(self.value)), node))
+        node.op = 'POW'
+        return node
+        
+    def __rpow__(self, other):
+        other = ReverseADNode(other)
+        return other.__pow__(self)
     
     def graph(self):
         if len(self.children) == 0:
-            print("leaf node: ", self.val )
+                print("leaf node of type: ", self.op, " and value: ", self.value)
         else:
-            print("Op node: ", self.op , " Value: ",  self.val)
+            print("Op node: ", self.op , " Value: ",  self.value)
             for der, node in self.children:
                 node.graph()
+    
+def sin(x):
+    try:
+        node = ReverseADNode(np.sin(x.value))
+        x.children.append((np.cos(x.value), node))
+        node.op = 'SIN'
+        return node
+    except AttributeError:
+        return np.sin(x)
 
-x = ReverseAutoDiff(4)
-y = ReverseAutoDiff(0.5)
-z = ReverseAutoDiff(5)
+def cos(x):
+    try:
+        node = ReverseADNode(np.cos(x.value))
+        x.children.append((-1*np.sin(x.value), node))
+        node.op = 'COS'
+        return node
+    except AttributeError:
+        return np.cos(x)
 
+def tan(x):
+    try:
+        node = ReverseADNode(np.tan(x.value))
+        x.children.append(((np.tan(x.value)**2)+1, node))
+        node.op = 'TAN'
+        return node
+    except AttributeError:
+        return np.tan(x)
+    
+def arcsin(x):
+    try:
+        node = ReverseADNode(np.arcsin(x.value))
+        x.children.append((1/np.sqrt(1-x.value**2), node))
+        node.op = 'ARCSIN'
+        return node
+    except AttributeError:
+        return np.arcsin(x)
 
-a = 4 + y * x 
-b = z - 2
-print("reverse mode res is ", a.reverse_mode())
-print("function value is ", a.val)
-print("reverse mode res is ", b.reverse_mode())
-print("function value is ", b.val)
+def arccos(x):
+    try:
+        node = ReverseADNode(np.arccos(x.value))
+        x.children.append((-1/np.sqrt(1-x.value**2), node))
+        node.op = 'ARCCOS'
+        return node
+    except AttributeError:
+        return np.arccos(x)
+    
+def arctan(x):
+    try:
+        node = ReverseADNode(np.arctan(x.value))
+        x.children.append((1/(1+x.value**2), node))
+        node.op = 'ARCTAN'
+        return node
+    except AttributeError:
+        return np.arctan(x)
 
-a.graph()
+def exp(x):
+    try:
+        node = ReverseADNode(np.exp(x.value))
+        x.children.append((np.exp(x.value), node))
+        node.op = 'EXP'
+        return node
+    except AttributeError:
+        return np.exp(x)
+
+def sinh(x):
+    try:
+        node = ReverseADNode(np.sinh(x.value))
+        x.children.append((np.cosh(x.value), node))
+        node.op = 'SINH'
+        return node
+    except AttributeError:
+        return np.sinh(x)
+
+def cosh(x):
+    try:
+        node = ReverseADNode(np.cosh(x.value))
+        x.children.append((np.sinh(x.value), node))
+        node.op = 'COSH'
+        return node
+    except AttributeError:
+        return np.cosh(x)
+
+def tanh(x):
+    try:
+        node = ReverseADNode(np.tanh(x.value))
+        x.children.append((1/(np.cosh(x.value)**2), node))
+        node.op = 'TANH'
+        return node
+    except AttributeError:
+        return np.tanh(x)
+    
+def log(x,base = np.exp):
+    try:
+        if x.value < 0:
+            raise ValueError('Log is not defined for negative values')
+        else:
+            node = ReverseADNode(np.log(x.value)/np.log(base))
+            x.children.append((1/(x.value*np.log(base)), node))
+            node.op = 'LOG'
+            return node
+    except AttributeError:
+        if x < 0:
+            raise ValueError('Log is not defined for negative values')
+        else: 
+            return np.log(x)/np.log(base)
+    
+def sqrt(x):
+    try:
+        if x.value < 0:
+            raise ValueError('Sqrt is not defined for negative values')
+        else:
+            node = ReverseADNode(np.sqrt(x.value))
+            x.children.append((1/(2*np.sqrt(x.value)), node))
+            node.op = 'SQRT'
+            return node
+    except AttributeError:
+        if x < 0:
+            raise ValueError('Sqrt is not defined for negative values')
+        else: 
+            return np.sqrt(x)
